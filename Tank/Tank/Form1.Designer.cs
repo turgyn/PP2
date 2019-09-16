eb461c6e188e0b38e33d4cb188243b81beaeef57
DarkhanTurgyn <55412780+DarkhanTurgyn@users.noreply.github.com>
2019-09-17 02:53:48 +0600

    Add files via upload
    Tank mini game for two players (without OOP)

---
+++ Tank/Tank/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Tank
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 467);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(298, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Tank.Resource1._6aRAd;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 489);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 489);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown1);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Tank/Tank/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tank
+{
+    public partial class Form1 : Form
+    {
+        enum dir
+        {
+            Up,
+            Right,
+            Down,
+            Left
+        }
+        SoundPlayer sp = new SoundPlayer(@"C:\Users\turgu\Downloads\shot.wav");
+        SoundPlayer sp2 = new SoundPlayer(@"C:\Users\turgu\Downloads\hit.wav");
+
+        dir dir1, dir2;
+        Size size = new Size(100, 100),
+            small = new Size(10, 10);
+        int score1 = 100, score2 = 100;
+
+        List<int> bullet_player1_x = new List<int>(),
+            bullet_player1_y = new List<int>(), 
+            bullet_player2_x = new List<int>(),
+            bullet_player2_y = new List<int>();
+
+        List<dir> dirbullet1 = new List<dir>();
+        List<dir> dirbullet2 = new List<dir>();
+
+        int bullet1 = -1 , bullet2 = -1;
+
+        Point position_player1 = new Point(0,0);
+        Point position_player2 = new Point(1265,580);
+
+        Rectangle rectangle1, rectangle2;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        Bitmap bm_p1, bm_p2;
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {   for(int i = 0; i < bullet_player1_x.Count; i++)
+            {
+                e.Graphics.FillEllipse(new SolidBrush(Color.Red), bullet_player1_x[i], bullet_player1_y[i], 10, 10);
+            }
+            for (int i = 0; i < bullet_player2_x.Count; i++)
+            {
+                e.Graphics.FillEllipse(new SolidBrush(Color.Blue), bullet_player2_x[i], bullet_player2_y[i], 10, 10);
+            }
+            e.Graphics.DrawImage(bm_p1, new Rectangle(position_player1, size));
+            e.Graphics.DrawImage(bm_p2, new Rectangle(position_player2, size));
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            bm_p1 = Resource1.player1_up;
+            bm_p2 = Resource1.player2_up;
+            rectangle1 = new Rectangle(position_player1, size);
+            rectangle2 = new Rectangle(position_player2, size);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for(int i = 0; i < bullet_player1_x.Count; i++)
+            {
+                switch (dirbullet1[i])
+                {
+                    case dir.Up:
+                        bullet_player1_y[i] -=10;
+                        break;
+                    case dir.Right:
+                        bullet_player1_x[i]+=10;
+                        break;
+                    case dir.Down:
+                        bullet_player1_y[i]+=10;
+                        break;
+                    case dir.Left:
+                        bullet_player1_x[i]-=10;
+                        break;
+                }
+                if(bullet_player1_x[i]<-10 || bullet_player1_x[i] > pictureBox1.Width+10 || bullet_player1_y[i] < -10 || bullet_player1_y[i] > pictureBox1.Height+10)
+                {//вылет пуль за окно
+                    bullet_player1_x.Remove(bullet_player1_x[i]);
+                    bullet_player1_y.Remove(bullet_player1_y[i]);
+                    dirbullet1.RemoveAt(i);
+                    
+                }
+                else if (bullet_player1_x[i] > position_player2.X && bullet_player1_x[i] < position_player2.X + 100 && bullet_player1_y[i] > position_player2.Y && bullet_player1_y[i] < position_player2.Y + 100)
+                {
+                    bullet_player1_x.Remove(bullet_player1_x[i]);
+                    bullet_player1_y.Remove(bullet_player1_y[i]);
+                    dirbullet1.RemoveAt(i);
+                    score1-=10;
+                    sp2.Play();
+                }
+
+            }
+            for (int i = 0; i < bullet_player2_x.Count; i++)
+            {
+                switch (dirbullet2[i])
+                {
+                    case dir.Up:
+                        bullet_player2_y[i] -= 10;
+                        break;
+                    case dir.Right:
+                        bullet_player2_x[i] += 10;
+                        break;
+                    case dir.Down:
+                        bullet_player2_y[i] += 10;
+                        break;
+                    case dir.Left:
+                        bullet_player2_x[i] -= 10;
+                        break;
+                }
+                if (bullet_player2_x[i] <= 0 || bullet_player2_x[i] > pictureBox1.Width + 10 || bullet_player2_y[i] < 0 || bullet_player2_y[i] > pictureBox1.Height + 10)
+                {
+                    bullet_player2_x.Remove(bullet_player2_x[i]);
+                    bullet_player2_y.Remove(bullet_player2_y[i]);
+                    dirbullet2.RemoveAt(i);
+                }
+                else if (bullet_player2_x[i] > position_player1.X && bullet_player2_x[i] < position_player1.X + 100 && bullet_player2_y[i] > position_player1.Y && bullet_player2_y[i] < position_player1.Y + 100)
+                {
+                    bullet_player2_x.Remove(bullet_player2_x[i]);
+                    bullet_player2_y.Remove(bullet_player2_y[i]);
+                    dirbullet2.RemoveAt(i);
+                    score2-=10;
+                    sp2.Play();
+                }
+            }
+            groupBox1.Text = "\n1 Player HP: " + score2 + "\n2 Player HP: " + score1;
+            if (score1 == 0)
+            {
+                while(MessageBox.Show("Player 1 Wins!!!") != DialogResult.OK)
+                {
+
+                }
+                score1 = -1;
+                timer1.Stop();
+            }
+            if (score2 ==0)
+            {
+                score2 = -1;
+                while (MessageBox.Show("Player 2 Wins!!!") != DialogResult.OK)
+                {
+
+                }
+                timer1.Stop();
+            }
+            else Refresh();
+            toolStripStatusLabel1.Text = string.Format("bullets1: {0} , bullters2 : {1}", bullet_player1_x.Count,bullet_player2_x.Count);
+        }
+
+        private void Form1_KeyDown1(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    dir1 = dir.Up;
+                    position_player1.Y-=5;
+                    break;
+                case Keys.D:
+                    dir1 = dir.Right;
+                    position_player1.X+=5;
+
+                    break;
+                case Keys.S:
+                    dir1 = dir.Down;
+                    position_player1.Y+=5;
+                    break;
+                case Keys.A:
+                    dir1 = dir.Left;
+                    position_player1.X-=5;
+                    break;
+                case Keys.Space:
+                    bullet1++;
+                    bullet_player1_x.Add(position_player1.X+45);
+                    bullet_player1_y.Add(position_player1.Y+45);
+                    //sp.Play();
+                    dirbullet1.Add(dir1);
+                    break;
+
+                case Keys.Up:
+                    dir2 = dir.Up;
+                    position_player2.Y -= 5;
+                    break;
+                case Keys.Right:
+                    dir2 = dir.Right;
+                    position_player2.X += 5;
+
+                    break;
+                case Keys.Down:
+                    dir2 = dir.Down;
+                    position_player2.Y += 5;
+                    break;
+                case Keys.Left:
+                    dir2 = dir.Left;
+                    position_player2.X -= 5;
+                    break;
+                case Keys.L:
+                    bullet2++;
+                    bullet_player2_x.Add(position_player2.X + 45);
+                    bullet_player2_y.Add(position_player2.Y + 45);
+                    //sp.Play();
+                    dirbullet2.Add(dir2);
+                    break;
+            }
+            changedir1(dir1);
+            changedir2(dir2);
+            
+            Refresh();
+        }
+        
+            private void changedir1(dir dir1)
+        {
+            switch (dir1)
+            {
+                case dir.Up:
+                    bm_p1 = Resource1.player1_up;
+                    break;
+                case dir.Right:
+                    bm_p1 = Resource1.player1_right;
+                    break;
+                case dir.Down:
+                    bm_p1 = Resource1.player1_down;
+                    break;
+                case dir.Left:
+                    bm_p1 = Resource1.player1_left;
+                    break;
+
+            }
+        }
+        private void changedir2(dir dir2)
+        {
+            switch (dir2)
+            {
+                case dir.Up:
+                    bm_p2 = Resource1.player2_up;
+                    break;
+                case dir.Right:
+                    bm_p2 = Resource1.player2_right;
+                    break;
+                case dir.Down:
+                    bm_p2 = Resource1.player2_down;
+                    break;
+                case dir.Left:
+                    bm_p2 = Resource1.player2_left;
+                    break;
+
+            }
+        }
+
+        private void Form1_KeyDown2(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
